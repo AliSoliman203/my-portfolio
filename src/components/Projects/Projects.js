440886bc395f9b13c988a84f97588e737a63c43c
AliSoliman203 <ali.soool1997@gmail.com>
2023-03-22 17:02:06 +0200

    update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -78,7 +78,7 @@ export const Projects = () => {
                                                 </Row>
                                             </Tab.Pane>
                                             <Tab.Pane eventKey="third">
-                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                                                <ProjectDesc>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</ProjectDesc>
                                             </Tab.Pane>
                                         </Tab.Content>
                                     </Tab.Container>
